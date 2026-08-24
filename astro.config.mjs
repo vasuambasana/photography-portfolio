09199d3800933c369ba2ce798a836dff1ce26e4c
Vasu Ambasana <vasu1998.va@gmail.com>
2026-08-24 12:51:58 -0400

    feat: add Google Analytics and Microsoft Clarity via Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://vasuambasana.com',
@@ -10,6 +11,11 @@ export default defineConfig({
     tailwind(),
     react(),
     sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push', 'gtag', 'clarity'],
+      },
+    }),
   ],
   image: {
     domains: [],
